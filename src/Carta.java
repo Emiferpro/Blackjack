d8e2417d3699f5800073a466f7035e15af02165a
emiferpro <emiferpro@gmail.com>
2024-10-11 00:59:00 -0300

    proyect: add images and gui

--- src/Carta.java
+++ src/Carta.java
@@ -1,2 +1,3 @@
 public class Carta {
+    // Initialize class components
 }
--- src/Juego.java
+++ src/Juego.java
@@ -1,2 +1,11 @@
+import javax.swing.*;
+
 public class Juego {
+    // Init GUI
+    public static JFrame frame = new JFrame("BlackJack");
+    public static void main(String[] args) {
+        frame.setSize(750, 600);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
 }
